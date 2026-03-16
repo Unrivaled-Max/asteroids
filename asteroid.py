c9cf5c6de43d0b579b2428f6756a822180349a66
boseeinstein <max@weareunrivaled.com>
2026-03-16 13:10:54 -0600

    Grading

---
+++ asteroid.py
@@ -0,0 +1,29 @@
+import pygame
+import random
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE_SECONDS, ASTEROID_KINDS
+from logger import log_event
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        rotation = random.uniform(20,50)
+        v1 = self.velocity.rotate(rotation)
+        v2 = self.velocity.rotate(-rotation)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = v1 * 1.2
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = v2 * 1.2
